61303abbbcba67b30f1a5fbb37786f9901fa0e5d
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-11 13:03:54 +1000

    Added bamboo_ut

---
+++ bamboo_ut.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -x
+
+PAPP_NAME="peek_agent"
+
+# activate virtualenv
+export PATH=/home/bamboo/pyenvs/py_ut/bin:$PATH
+
+# Define python path
+PYTHONPATH="`pwd`/peek_papp/src"
+PYTHONPATH="$PYTHONPATH:`pwd`/rapui/src"
+PYTHONPATH="$PYTHONPATH:`pwd`/${PAPP_NAME}/src"
+export PYTHONPATH
+
+
+FILES=`find $UT_DIRS -name "*.py" -exec grep -l unittest.TestCase {} \;`
+echo "Running unit tests in files:"
+echo $FILES
+
+JUNIT_DIR=.junit
+mkdir ${JUNIT_DIR}
+OUT=${JUNIT_DIR}/trial.xml
+
+# Trial exists non-zero if there are failed tests, we want to ignore this
+trial --reporter=subunit ${FILES} | subunit-1to2 | subunit2junitxml -o $OUT || true
+
+echo "Finished"
+
+
