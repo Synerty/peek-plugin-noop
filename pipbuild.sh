34f0c4d18bbf715f7bcf03b668c3494126cd463c
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-09 15:57:41 +1000

    Changed setver to pipbuild

---
+++ pipbuild.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+PACKAGE="papp_noop"
+
+set -o nounset
+set -o errexit
+#set -x
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "There are uncomitted changes, please make sure all changes are comitted" >&2
+    exit 1
+fi
+
+if ! [ -f "setup.py" ]; then
+    echo "setver.sh must be run in the directory where setup.py is" >&2
+    exit 1
+fi
+
+VER="${1:?You must pass a version of the format 0.0.0 as the only argument}"
+
+if git tag | grep -q "${VER}"; then
+    echo "Git tag for version ${VER} already exists." >&2
+    exit 1
+#    read -p "Git tag for version ${VER} already exists, do you want to overwrite (Y/N) ? " -n 1 -r
+#    echo    # (optional) move to a new line
+#    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+#        echo "Aborting, version already exists"
+#        exit 1
+#    fi
+fi
+
+echo "Setting version to $VER"
+
+DOWNLOAD_URL="    download_url='"
+DOWNLOAD_URL="${DOWNLOAD_URL}https://github.com/Synerty/${PACKAGE}/tarball/${VER}',"
+
+VERSION="    version='"
+VERSION="${VERSION}${VER}',"
+
+# Update the setup.py
+sed -i "s;.* download_url=.*;${DOWNLOAD_URL};"  setup.py
+sed -i "s;.* version=.*;${VERSION};"  setup.py
+
+# Update the package version
+sed -i "s;.*version.*;__version__ = '${VER}';" ${PACKAGE}/__init__.py
+
+python setup.py sdist
+
+# Reset the commit, we don't want versions in the commit
+git reset --hard
+
+git tag ${VER}
+git pus --tags
+
