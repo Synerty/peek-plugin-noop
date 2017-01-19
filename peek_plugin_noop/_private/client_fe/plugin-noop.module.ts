6e7a19b56087810c8aa9c0ba6790be03878e75e6
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-19 21:37:37 +1000

    Updating cross plaform module switching for forms module

--- peek_plugin_noop/_private/client_fe/plugin-noop.module.ts
+++ peek_plugin_noop/_private/client_fe/plugin-noop.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {PluginNoopClientComponent} from "./plugin-noop-client.component";
 import {Routes, RouterModule} from "@angular/router";
-import {PeekRouterModule} from "@synerty/peek-web-ns";
+import {PeekModuleFactory} from "@synerty/peek-web-ns";
 // import {PeekPluginMenuI, PeekPluginMenuItem} from "interfaces/PeekPluginMenuItem";
 /**
  * Created by peek on 5/12/16.
@@ -26,7 +26,7 @@ export const pluginRoutes: Routes = [
 @NgModule({
     imports: [
         CommonModule,
-        PeekRouterModule.forChild(pluginRoutes)],
+        PeekModuleFactory.RouterModule.forChild(pluginRoutes)],
     exports: [],
     providers: [],
     declarations: [PluginNoopClientComponent]
