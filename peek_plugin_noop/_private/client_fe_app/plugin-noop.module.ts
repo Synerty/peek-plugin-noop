ed76c758f4394ce37db80eeb58c39b7936927b7a
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-11 00:11:14 +1000

    Nativescript and web frontends now build and run.

--- peek_plugin_noop/_private/client_fe_app/plugin-noop.module.ts
+++ peek_plugin_noop/_private/client_fe_app/plugin-noop.module.ts
@@ -2,7 +2,7 @@ import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {PluginNoopClientComponent} from "./plugin-noop-client.component";
 import {Routes, RouterModule} from "@angular/router";
-import {PeekModuleFactory} from "@synerty/peek-web-ns";
+import {PeekModuleFactory} from "@synerty/peek-web-ns/index.web";
 // import {PeekPluginMenuI, PeekPluginMenuItem} from "interfaces/PeekPluginMenuItem";
 /**
  * Created by peek on 5/12/16.
