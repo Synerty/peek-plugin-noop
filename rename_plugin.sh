06be7d575dcd8ef48c61563b0cf00c2a51005313
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-17 09:16:23 +1000

    Changed rename_plugin to rename depth first
    This avoids having to run it twice.

--- rename_plugin.sh
+++ rename_plugin.sh
@@ -21,7 +21,7 @@ function replace {
     from=$1
     to=$2
 
-    for d in `find ./ -type d`
+    for d in `find ./ -type d -depth`
     do
         new=`echo $d | sed "s/$from/$to/g"`
         [ "$d" != "$new" ] && mv $d $new
