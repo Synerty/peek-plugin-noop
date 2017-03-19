9c4b2795afb18dcd2dedc36eb0071d9df9cbb17f
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-03-19 15:59:20 +1000

    Refactoring package to match new standards in document

---
+++ peek_plugin_noop/_private/admin-app/noop.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'noop-admin',
+    templateUrl: 'noop.component.html'
+})
+export class NoopComponent  implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
--- peek_plugin_noop/_private/admin-app/noop.module.ts
+++ peek_plugin_noop/_private/admin-app/noop.module.ts
@@ -1,16 +1,13 @@
 import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
-import {PluginNoopAdminComponent} from "./plugin-noop-admin.component";
+import {NoopComponent} from "./noop.component";
 import {Routes, RouterModule} from "@angular/router";
-/**
- * Created by peek on 5/12/16.
- *
- */
+
 
 export const pluginRoutes: Routes = [
     {
         path: '',
-        component: PluginNoopAdminComponent
+        component: NoopComponent
     }
 
 ];
@@ -21,8 +18,8 @@ export const pluginRoutes: Routes = [
         RouterModule.forChild(pluginRoutes)],
     exports: [],
     providers: [],
-    declarations: [PluginNoopAdminComponent]
+    declarations: [NoopComponent]
 })
-export class PluginNoopAdminModule {
+export class NoopModule {
 
 }
