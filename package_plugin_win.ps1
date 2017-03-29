b4edf7e9a840cba5275e498f3c12dd2163d15da9
Brenton Ford <brenton.ford@synerty.com>
2017-03-28 21:36:05 -0400

    Will packaged a peek-plugin from pypi
    ORMOB-98

---
+++ package_plugin_win.ps1
@@ -0,0 +1,67 @@
+param(
+    [String]$wantedVer,
+    [Parameter(Mandatory=$true)]
+    [String]$plugin)
+
+Write-Host "Requested peek plugin is $plugin"
+# Replace String with underscores
+$pluginUnder=$plugin -replace '-','_'
+
+# Make Powershell stop if it has errors
+$ErrorActionPreference = "Stop"
+
+if (-Not [string]::IsNullOrEmpty($wantedVer)) {
+    Write-Host "Requested version is $wantedVer"
+}
+
+# Get the current location
+$startDir=Get-Location
+
+$baseDir="$startDir\peek_dist_win_$pluginUnder";
+
+# Delete the existing dist dir if it exists
+If (Test-Path $baseDir){
+    Remove-Item $baseDir -Force -Recurse;
+}
+
+# Create our new dist dir
+New-Item $baseDir -ItemType directory;
+
+# ------------------------------------------------------------------------------
+# Download the peek platform and all it's dependencies
+New-Item "$baseDir\py" -ItemType directory;
+Set-Location "$baseDir\py";
+Write-Host "Downloading and creating windows wheels";
+pip wheel --no-cache $plugin;
+
+# Make sure we've downloaded the right version
+$peekPkgName = Get-ChildItem ".\" |
+                    Where-Object {$_.Name.StartsWith("$pluginUnder-")} |
+                    Select-Object -exp Name;
+$peekPkgVer = $peekPkgName.Split('-')[1];
+
+if (-Not [string]::IsNullOrEmpty($wantedVer) -and $peekPkgVer -ne $wantedVer) {
+    Set-Location "$startDir";
+    Write-Error "We've downloaded version $peekPkgVer, but you wanted ver $wantedVer";
+} else {
+    Write-Host "We've downloaded version $peekPkgVer";
+}
+
+# ------------------------------------------------------------------------------
+# Set the location back to where we were.
+Set-Location $startDir;
+
+# Finally, version the directory
+$releaseDir="$($baseDir)_$($peekPkgVer)";
+$relaseZip="$($releaseDir).zip"
+Move-Item $baseDir $releaseDir -Force;
+
+# Create the zip file
+Add-Type -Assembly System.IO.Compression.FileSystem;
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal;
+[System.IO.Compression.ZipFile]::CreateFromDirectory(
+    $releaseDir, $relaseZip, $compressionLevel, $false)
+
+# We're all done.
+Write-Host "Successfully created release $peekPkgVer";
+Write-Host "Located at $relaseZip";
