efb1c0e9b5fb26922b891f81cbbcb3370697ed4b
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-06 19:27:32 +1000

    Updated to version 1.3.1.1

--- peek_plugin_noop/__init__.py
+++ peek_plugin_noop/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.0'
+__version__ = '1.3.1.1'
 
 from peek_plugin_base.agent.PluginAgentEntryHookABC import PluginAgentEntryHookABC
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
