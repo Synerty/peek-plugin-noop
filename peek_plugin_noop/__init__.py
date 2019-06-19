f5475c1f7ff69e8838fcc06c3f927d3f39fe8883
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-19 14:53:26 +1000

    Updated package publish scripts.
    PEEK-321

--- peek_plugin_noop/__init__.py
+++ peek_plugin_noop/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.2'
+__version__ = '0.0.0'
 
 from peek_plugin_base.agent.PluginAgentEntryHookABC import PluginAgentEntryHookABC
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
---
+++ publish.settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+PY_PACKAGE="peek_plugin_noop"
+PYPI_PUBLISH="0"
+
+VER_FILES=""
