09ca7f7418cd2faddf4997573c584d6e218dff9d
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-08-21 11:02:02 +1000

    PEEK-662: Replaced Font Awesome icons with Ant Design icons.

--- peek_plugin_noop/_private/mobile-app/noop.module.ts
+++ peek_plugin_noop/_private/mobile-app/noop.module.ts
@@ -1,9 +1,7 @@
 import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {Routes} from "@angular/router";
-
-// Import a small abstraction library to switch between nativescript and web
-import {PeekModuleFactory} from "@synerty/peek-util-web";
+import {RouterModule} from "@angular/router";
 
 // Import the default route component
 import {NoopComponent} from "./noop.component";
@@ -29,15 +27,14 @@ export const pluginRoutes: Routes = [
 
 // Define the root module for this plugin.
 // This module is loaded by the lazy loader, what ever this defines is what is started.
-// When it first loads, it will look up the routs and then select the component to load.
+// When it first loads, it will look up the routes and then select the component to load.
 @NgModule({
     imports: [
         CommonModule,
-        PeekModuleFactory.RouterModule.forChild(pluginRoutes)],
+        RouterModule.forChild(pluginRoutes)
+    ],
     exports: [],
     providers: [],
     declarations: [NoopComponent, ConfigComponent]
 })
-export class NoopModule
-{
-}
+export class NoopModule {}
--- peek_plugin_noop/plugin-module/_private/mobile/plugin-noop-root.module.ts
+++ peek_plugin_noop/plugin-module/_private/mobile/plugin-noop-root.module.ts
@@ -1,28 +1,24 @@
-import {NgModule, OnDestroy} from "@angular/core";
-import {PluginNoopClientRootService} from "./plugin-noop-root.service";
-import {CommonModule} from "@angular/common";
-import {PeekModuleFactory} from "@synerty/peek-util-web";
-import {MobileRootComponent} from "./root-component/root.component";
+import { NgModule, OnDestroy } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { PluginNoopClientRootService } from "./plugin-noop-root.service";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { MobileRootComponent } from "./root-component/root.component";
 
 /** Client Root Module
  *
- * This module will be loaded by peek-mobile when the app laods.
+ * This module will be loaded by peek-mobile when the app loads.
  * There will be one instance of it, and it be around for the life of the app.
  *
  * Configure this in plugin_package.json
  */
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ...PeekModuleFactory.FormsModules,
-    ],
-    exports: [
-        MobileRootComponent
-    ],
-    providers: [
-    ],
-    declarations: [MobileRootComponent]
+    imports: [CommonModule, FormsModule, NzIconModule, HttpClientModule],
+    exports: [MobileRootComponent],
+    providers: [],
+    declarations: [MobileRootComponent],
 })
 export class PluginNoopClientRootModule implements OnDestroy {
     private static instanceCount = 0;
@@ -30,12 +26,16 @@ export class PluginNoopClientRootModule implements OnDestroy {
 
     constructor(private noopRootService: PluginNoopClientRootService) {
         this.instanceIndex = PluginNoopClientRootModule.instanceCount++;
-        console.log("peek-plugin-noop - PluginNoopClientRootModule LOADED #"
-            + this.instanceIndex);
+        console.log(
+            "peek-plugin-noop - PluginNoopClientRootModule LOADED #" +
+                this.instanceIndex
+        );
     }
 
     ngOnDestroy() {
-        console.log("peek-plugin-noop - PluginNoopClientRootModule DESTROYED #"
-            + this.instanceIndex);
+        console.log(
+            "peek-plugin-noop - PluginNoopClientRootModule DESTROYED #" +
+                this.instanceIndex
+        );
     }
 }
